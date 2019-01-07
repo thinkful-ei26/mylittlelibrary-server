d4e687d16eb48ccfae3fef32f96e1ce3f6fc08e7
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-07 08:49:07 -0700

    setup routes for users and authentication in main router module index.js

--- config.js
+++ config.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-      process.env.DATABASE_URL || 'mongodb://localhost/library-app',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/library-app',
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/thinkful-backend-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || 60 * 10 * 1000
 };
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 const morgan = require('morgan');
+const passport = require('passport');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+mongoose.Promise = global.Promise;
+
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { Book } = require('./models/test_model');
 const { dbConnect } = require('./db-mongoose');
+
 const app = express();
 
 app.use(
@@ -16,13 +23,53 @@ app.use(
   })
 );
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
-app.use(bodyParser.json());
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use(bodyParser.json());
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+/****************AUTHENTICATION ROUTES***************** */
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+
+
+
+
+
+
+
+
+
+
+/***************AUTHENTICATION ROUTES END**************** */
 app.get('/books/', (req, res) => {
   let query = {};
   // const {genericQuery} =req.query;
